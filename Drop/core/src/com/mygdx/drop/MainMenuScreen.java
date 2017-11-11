efc8a1a822eb891e6ff2e7971ae4dbbb0bd3d80a
Iqbal Ahmed (Sinbad) <ask2iqbal@gmail.com>
2017-11-11 14:59:24 +0800

    Completed libGdx wiki 2nd Tutorial

---
+++ Drop/core/src/com/mygdx/drop/MainMenuScreen.java
@@ -0,0 +1,72 @@
+package com.mygdx.drop;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+/**
+ * Created by dastaniqbal on 11/11/2017.
+ * dastanIqbal@marvelmedia.com
+ * 11/11/2017 2:41
+ */
+
+class MainMenuScreen implements Screen {
+    final Drop game;
+    private OrthographicCamera camera;
+
+    public MainMenuScreen(final Drop game) {
+        this.game = game;
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.font.draw(game.batch, "Welcome to Drop!!!", 100, 150);
+        game.font.draw(game.batch, "Tap anwyare to begin!", 100, 100);
+        game.batch.end();
+
+        if(Gdx.input.isTouched()){
+            game.setScreen(new GameScreen(game));
+            dispose();
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
