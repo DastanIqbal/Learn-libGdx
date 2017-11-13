216d32299b460a690ebddd880fb50ed2fa9327d8
Iqbal Ahmed (Sinbad) <ask2iqbal@gmail.com>
2017-11-13 22:18:47 +0800

    Box2D intro

---
+++ box2dtut/core/src/blog/gamedevelopmentbox2dtutorial/model/B2DModel.kt
@@ -0,0 +1,102 @@
+package blog.gamedevelopmentbox2dtutorial.model
+
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.physics.box2d.*
+
+/**
+ * Created by dastaniqbal on 12/11/2017.
+ * dastanIqbal@marvelmedia.com
+ * 12/11/2017 6:47
+ */
+class B2DModel {
+    val world = World(Vector2(0f, -10f), true)
+
+    init {
+        createFloor()
+        createObject()
+        createMovingObject()
+    }
+
+    fun logicStep(delta: Float) {
+        world.step(delta, 3, 3)
+    }
+
+    private var bodyd: Body? = null
+
+    private fun createObject() {
+        //create a new body definition (type and location)
+        val bodyDef = BodyDef()
+        bodyDef.type = BodyDef.BodyType.DynamicBody
+        bodyDef.position.set(0f, 0f)
+
+        // add it to the world
+        bodyd = world.createBody(bodyDef)
+
+        // set the shape (here we use a box 50 meters wide, 1 meter tall )
+        val shape = PolygonShape()
+        shape.setAsBox(1f, 1f)
+
+        // set the properties of the object ( shape, weight, restitution(bouncyness)
+        val fixtureDef = FixtureDef()
+        fixtureDef.shape = shape
+        fixtureDef.density = 1f
+
+        // create the physical object in our body)
+        // without this our body would just be data in the world
+        bodyd?.createFixture(shape, 0.0f)
+
+        // we no longer use the shape object here so dispose of it.
+        shape.dispose()
+    }
+
+    private var bodys: Body? = null
+
+    private fun createFloor() {
+        //create a new body definition (type and location)
+        val bodyDef = BodyDef()
+        bodyDef.type = BodyDef.BodyType.StaticBody
+        bodyDef.position.set(0f, -10f)
+
+        // add it to the world
+        bodys = world.createBody(bodyDef)
+
+        // set the shape (here we use a box 50 meters wide, 1 meter tall )
+        val shape = PolygonShape()
+        shape.setAsBox(50f, 1f)
+
+        // create the physical object in our body)
+        // without this our body would just be data in the world
+        bodys?.createFixture(shape, 0.0f)
+
+        // we no longer use the shape object here so dispose of it.
+        shape.dispose()
+    }
+
+    private var bodyk: Body? = null
+    private fun createMovingObject() {
+        //create a new body definition (type and location)
+        val bodyDef = BodyDef()
+        bodyDef.type = BodyDef.BodyType.KinematicBody
+        bodyDef.position.set(0f, -12f)
+
+        // add it to the world
+        bodyk = world.createBody(bodyDef)
+
+        // set the shape (here we use a box 50 meters wide, 1 meter tall )
+        val shape = PolygonShape()
+        shape.setAsBox(1f, 1f)
+
+        // set the properties of the object ( shape, weight, restitution(bouncyness)
+        val fixtureDef = FixtureDef()
+        fixtureDef.shape = shape
+        fixtureDef.density = 1f
+
+        // create the physical object in our body)
+        // without this our body would just be data in the world
+        bodyk?.createFixture(shape, 0.0f)
+
+        // we no longer use the shape object here so dispose of it.
+        shape.dispose()
+        bodyk?.setLinearVelocity(0f, 0.75f)
+    }
+}
--- box2dtut/core/src/blog/gamedevelopmentbox2dtutorial/views/MainScreen.kt
+++ box2dtut/core/src/blog/gamedevelopmentbox2dtutorial/views/MainScreen.kt
@@ -1,7 +1,12 @@
 package blog.gamedevelopmentbox2dtutorial.views
 
 import blog.gamedevelopmentbox2dtutorial.Box2DTutorial
+import blog.gamedevelopmentbox2dtutorial.model.B2DModel
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Screen
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer
 
 /**
  * Created by dastaniqbal on 12/11/2017.
@@ -10,13 +15,19 @@ import com.badlogic.gdx.Screen
  */
 
 class MainScreen(private val parent: Box2DTutorial) : Screen {
+    val model = B2DModel()
+    val cam = OrthographicCamera(32f, 24f)
+    val debugRenderer = Box2DDebugRenderer(true, true, true, true, true, true)
 
     override fun show() {
 
     }
 
     override fun render(delta: Float) {
-
+        model.logicStep(delta)
+        Gdx.gl.glClearColor(0f,0f,0f,1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        debugRenderer.render(model.world,cam.combined)
     }
 
     override fun resize(width: Int, height: Int) {
